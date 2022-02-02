3a1daa699d310394cd455294f47260d2230764b8
grsmith35 <gordonrileysmith@gmail.com>
2022-02-02 13:12:27 -0700

    fixed error with route

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
           <Switch>
             <Route exact path="/" component={Home} />
             <Route exact path="/yeshelpers/Payroll Converter" component={Payroll} />
-            <Route exact path="/yes/helpers/Resume Builder" component={Resume} />
+            <Route exact path="/yeshelpers/Resume Builder" component={Resume} />
             <Route exact path="/Home" component={Home} />
              
             <Route component={Home} />
